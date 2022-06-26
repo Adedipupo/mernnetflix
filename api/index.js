5e7bea120a6fd505974eb5ca38ea2fdcf1920cc0
Adedipupo <ajokanola@terragonltd.com>
2022-06-26 18:34:25 +0100

    auth

--- api/index.js
+++ api/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const authRoute = require('./routes/auth');
+
 const app = express();
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true, 
 }).then(()=>console.log("Database conneected")).catch((err)=>console.log(err))
 
+app.use(express.json());
+app.use("/ api/auth", authRoute);
 
 app.listen(8800,()=>{
     console.log("Server is runnning on on 8800 ")
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const CryptoJS = require("crypto-js");
+// const CryptoJS = require("crypto-js");
 const jwt = require("jsonwebtoken");
 
 
@@ -16,3 +16,5 @@ router.post("/register",(req,res) => {
     const user = newUSer.save();
     res.json(user);
 })
+
+module.exports = router;
