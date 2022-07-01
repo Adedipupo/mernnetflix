de1bba83800c11160f2e5b1c3c5a027f001b760d
Adedipupo <ajokanola@terragonltd.com>
2022-07-01 02:52:13 +0100

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,21 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Home/> */}
-      {/* <Watch/> */}
-      {/* <Register/> */}
-      <Login/>
+      <BrowserRouter>
+        <Routes>
+        <Route index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
