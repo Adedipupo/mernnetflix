85ffab263687f7c0378f956af01f88c0085d1779
Adedipupo <ajokanola@terragonltd.com>
2022-07-01 10:01:33 +0100

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,33 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  const user = true;
+  const user = false;
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-        <Route index element={user ? <Home /> : <Navigate to="/register" />} />
-        <Route path="register" element={!user ? <Register /> : <Navigate to="/login" />} />
-        <Route path="login" element={<Login />} />
-        <Route path="/movies" element={<Home type="movies" />} />
-        <Route path="/series" element={<Home type="series" />} />
-        <Route path="/watch" element={<Watch />} />
+          <Route
+            index
+            element={user ? <Home /> : <Navigate to="/register" />}
+          />
+          <Route
+            path="register"
+            element={!user ? <Register /> : <Navigate to="/" />}
+          />
+          <Route
+            path="login"
+            element={!user ? <Login /> : <Navigate to="/" />}
+          />
+          {user && (
+            <>
+              <Route path="/movies" element={<Home type="movies" />} />
+              <Route path="/series" element={<Home type="series" />} />
+              <Route path="/watch" element={<Watch />} />
+            </>
+          )}
         </Routes>
       </BrowserRouter>
     </div>
