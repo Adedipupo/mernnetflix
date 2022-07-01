9ea3d4a94e2442ffd3a757934f845974622bb82b
Adedipupo <ajokanola@terragonltd.com>
2022-07-01 10:56:49 +0100

    wip

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List";
 import Navbar from "../../components/Navbar/Navbar";
 import "./home.scss";
+import axios from "axios"
 
 const Home = ({type}) => {
+  const [lists,setLists] = useState([]);
+
+  useEffect(()=> {
+    const getRandom = async () => {
+      try {
+        const res = await axios.get("/lists");
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  })
   return (
     <div className="home">
       <Navbar/>
