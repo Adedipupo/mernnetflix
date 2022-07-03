5e052d547a25eebd2858c0e1380a3343802ac7f4
Adedipupo <ajokanola@terragonltd.com>
2022-07-03 18:16:52 +0100

    wip

--- admin/src/components/widgetSm/WigdetSm.jsx
+++ admin/src/components/widgetSm/WigdetSm.jsx
@@ -1,79 +1,47 @@
 import { Visibility } from "@material-ui/icons";
+import { useEffect } from "react";
+import { useState } from "react";
 import "./widgetsm.css";
+import axios from "axios";
 
 export default function WigdetSm() {
+ const [newUser, setNewUser] = useState([])
+
+  useEffect(() => {
+    const getNewUser = async () => {
+      try {
+        const res = await axios.get("/users?new=true", {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYjliNDhlYTM5YzE4YjQxMzI0OTFkZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1Njg2NjI4MCwiZXhwIjoxNjU3Mjk4MjgwfQ.SEQpEEFxbtyLH96JNmu--QxGLhUIm6rYsie0SbuJ6IU",
+          },
+        });
+        setNewUser(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getNewUser();
+  }, []);
+  console.log("first", newUser);
   return (
     <div className="smallWidget">
       <span className="smallWigdetTitle">
         Newly Join Members
       </span>
       <ul className="smallWigdetList">
+        {newUser.map((user) =>( 
         <li className="smallWigdetListItem">
-        <img src="https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png" alt="" className="smallWigdetImage" />
+        <img src={user.profilePic || "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"} alt="" className="smallWigdetImage" />
         <div className="smallWigdetUser">
-          <span className="smallWigdetUsername">Dipo Dipo</span>
-          <span className="smallWigdetUserTitle">Hustling</span>
+          <span className="smallWigdetUsername">{user.username}</span>
         </div>
         <button className="smallWigdetButton">
           <Visibility className="smallWigdetIcon"/>
           Display
         </button>
         </li>
-        <li className="smallWigdetListItem">
-        <img src="https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png" alt="" className="smallWigdetImage" />
-        <div className="smallWigdetUser">
-          <span className="smallWigdetUsername">Dipo Dipo</span>
-          <span className="smallWigdetUserTitle">Hustling</span>
-        </div>
-        <button className="smallWigdetButton">
-          <Visibility className="smallWigdetIcon"/>
-          Display
-        </button>
-        </li>
-        <li className="smallWigdetListItem">
-        <img src="https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png" alt="" className="smallWigdetImage" />
-        <div className="smallWigdetUser">
-          <span className="smallWigdetUsername">Dipo Dipo</span>
-          <span className="smallWigdetUserTitle">Hustling</span>
-        </div>
-        <button className="smallWigdetButton">
-          <Visibility className="smallWigdetIcon"/>
-          Display
-        </button>
-        </li>
-        <li className="smallWigdetListItem">
-        <img src="https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png" alt="" className="smallWigdetImage" />
-        <div className="smallWigdetUser">
-          <span className="smallWigdetUsername">Dipo Dipo</span>
-          <span className="smallWigdetUserTitle">Hustling</span>
-        </div>
-        <button className="smallWigdetButton">
-          <Visibility className="smallWigdetIcon"/>
-          Display
-        </button>
-        </li>
-        <li className="smallWigdetListItem">
-        <img src="https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png" alt="" className="smallWigdetImage" />
-        <div className="smallWigdetUser">
-          <span className="smallWigdetUsername">Dipo Dipo</span>
-          <span className="smallWigdetUserTitle">Hustling</span>
-        </div>
-        <button className="smallWigdetButton">
-          <Visibility className="smallWigdetIcon"/>
-          Display
-        </button>
-        </li>
-        <li className="smallWigdetListItem">
-        <img src="https://www.kindpng.com/picc/m/78-786207_user-avatar-png-user-avatar-icon-png-transparent.png" alt="" className="smallWigdetImage" />
-        <div className="smallWigdetUser">
-          <span className="smallWigdetUsername">Dipo Dipo</span>
-          <span className="smallWigdetUserTitle">Hustling</span>
-        </div>
-        <button className="smallWigdetButton">
-          <Visibility className="smallWigdetIcon"/>
-          Display
-        </button>
-        </li>
+        ))}
       </ul>
     </div>
   )
--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -54,7 +54,6 @@ export default function Home() {
     };
     getStats();
   }, [MONTHS]);
-  console.log(userStats);
   return (
     <div className="home">
       <FeaturedInfo/>
